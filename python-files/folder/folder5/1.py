bbf96ee3eb870c6264abfe2e9b2eb5f2321af079
Mohsin7678-cell <mohsin7678670@gmail.com>
2026-02-20 16:13:30 +0530

    update

--- python-files/folder/folder5/1.py
+++ python-files/folder/folder5/1.py
@@ -2,3 +2,7 @@
 print("hrllo")
 git@github.com:Mohsin7678-cell/python-project.git (fetch)
 git@github.com:Mohsin7678-cell/python-project.git (push)
+
+x=input("enter name - ")
+y=input("enter city name - ")
+print(x, "lives in" ,y)
