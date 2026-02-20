3cf302164b3072a4c6e4d10aa10bb4e1ba26304a
Mohsin7678-cell <mohsin7678670@gmail.com>
2026-02-20 15:35:07 +0530

    update

--- python-files/folder/folder1/function.py
+++ python-files/folder/folder1/function.py
@@ -1,18 +1,34 @@
 # case : 1 - without argument witout return : takes nothing give nothing
 
-def addition():    ## function definition
-  '''this is for addition of two numbers'''    ## optional
-n1 = int(input("enter first number"))
-n2 = int(input("enter second number"))
-a = n1 + n2
-b = n1 - n2
-c = n1 * n2
-d = n1 / n2
-print("sum of n1 and n2 is",a)
+# def addition():    ## function definition
+#   '''this is for addition of two numbers'''    ## optional
+# n1 = int(input("enter first number"))
+# n2 = int(input("enter second number"))
+# a = n1 + n2
+# b = n1 - n2
+# c = n1 * n2
+# d = n1 / n2
+# print("sum of n1 and n2 is",a)
 # print("sub of n1 and n2 is",b)
 # print("mul of n1 and n2 is",c)
 # print("div of n1 and n2 is",d)
 
+# print ("hii")
+# addition()      ## calling
 
-print ("hii")
-addition()      ## calling
+
+# case : 2 - with argument without return type
+
+# def addition(n1,n2):  # formal argument
+#     '''this is for addition of two numbers i.e, n1 and n2'''
+#     r = n1 + n2
+#     print("sum is",r)
+    
+# # addition(2,7)
+#                                 # or
+# num1=int(input("enter first number"))
+# num2=int(input("enter second number"))
+# addition(num1,num2)    # actual argument
+
+
+# case : 3 - with argument with return
